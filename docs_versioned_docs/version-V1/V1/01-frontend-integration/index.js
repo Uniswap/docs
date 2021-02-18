1ccc6dbe68e1b25aa1f125ac34492322da969c82
Connor Martin <cnr.mrtn@gmail.com>
2021-02-18 12:09:39 -0600

    idk

--- docs_versioned_docs/version-V1/V1/01-frontend-integration/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Redirect } from '@reach/router'
-
-export default function Redirector({ path }) {
-  return <Redirect from={path} to={`${path}connect-to-uniswap/`} noThrow />
-}
--- sidebars.js
+++ sidebars.js
@@ -1,171 +1,43 @@
 module.exports = {
   
     docs: [
-    {
-      type: 'category',
-      label:'Protocol Overview',
-      items: [
-        'concepts/01-protocol-overview/01-how-uniswap-works',
-        'concepts/01-protocol-overview/02-ecosystem-participants',
-        'concepts/01-protocol-overview/03-smart-contracts',
-        'concepts/01-protocol-overview/04-glossary',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Core Concepts',
-      items: [
-        'concepts/02-core-concepts/01-swaps',
-        'concepts/02-core-concepts/02-pools',
-        'concepts/02-core-concepts/03-flash-swaps',
-        'concepts/02-core-concepts/04-oracles',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Advanced Topics',
-      items: [
-        'concepts/03-advanced-topics/01-fees',
-        'concepts/03-advanced-topics/02-pricing',
-        'concepts/03-advanced-topics/03-understanding-returns',
-        'concepts/03-advanced-topics/04-security',
-        'concepts/03-advanced-topics/05-math',
-        'concepts/03-advanced-topics/06-research'
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Governance',
-      items: [
-        'concepts/11-governance/01-overview',
-        'concepts/11-governance/02-process',
-        'concepts/11-governance/03-guide-to-voting',
-        'concepts/11-governance/04-glossary',
-        'concepts/11-governance/05-adversarial-circumstances',
-        'concepts/11-governance/06-governance-reference',
-      ],
-    },
+    
     {
       type: 'category',
       label: 'Frontend Integration',
       items: [
-        'V1/01-frontend-integration/01-connect-to-uniswap',
-        'V1/01-frontend-integration/02-pool-liquidity',
-        'V1/01-frontend-integration/03-trade-tokens',
-        'V1/01-frontend-integration/04-custom-linking',
-        'V1/01-frontend-integration/05-iframe-integration',
-        'V1/01-frontend-integration/06-token-listing',
+        'V1/V1/01-frontend-integration/01-connect-to-uniswap',
+        'V1/V1/01-frontend-integration/02-pool-liquidity',
+        'V1/V1/01-frontend-integration/03-trade-tokens',
+        'V1/V1/01-frontend-integration/04-custom-linking',
+        'V1/V1/01-frontend-integration/05-iframe-integration',
+        'V1/V1/01-frontend-integration/06-token-listing',
       ],
     },
     {
       type: 'category',
       label: 'SDK',
       items: [
-        'V1/02-SDK/01-getting-started',
-        'V1/02-SDK/02-data',
-        'V1/02-SDK/03-computation',
-        'V1/02-SDK/04-format',
-        'V1/02-SDK/05-orchestration',
-        'V1/02-SDK/06-transact',
-        'V1/02-SDK/07-constants',
-        'V1/02-SDK/08-types',
+        'V1/V1/02-SDK/01-getting-started',
+        'V1/V1/02-SDK/02-data',
+        'V1/V1/02-SDK/03-computation',
+        'V1/V1/02-SDK/04-format',
+        'V1/V1/02-SDK/05-orchestration',
+        'V1/V1/02-SDK/06-transact',
+        'V1/V1/02-SDK/07-constants',
+        'V1/V1/02-SDK/08-types',
       ],
     },
     {
       type: 'category',
       label: 'Smart Contracts',
       items: [
-        'V1/03-smart-contracts/01-factory',
-        'V1/03-smart-contracts/02-exchange',
-        'V1/03-smart-contracts/03-interfaces',
+        'V1/V1/03-smart-contracts/01-factory',
+        'V1/V1/03-smart-contracts/02-exchange',
+        'V1/V1/03-smart-contracts/03-interfaces',
       ],
     },
   ],
 
-
-  guides: [
-    {
-      type: 'category',
-      label: 'Smart Contract Integration',
-      items: [
-        'guides/smart-contract-integration/01-quick-start',
-        'guides/smart-contract-integration/02-trading-from-a-smart-contract',
-        'guides/smart-contract-integration/03-providing-liquidity',
-        'guides/smart-contract-integration/04-building-an-oracle',
-        'guides/smart-contract-integration/05-using-flash-swaps',
-        'guides/smart-contract-integration/06-getting-pair-addresses',
-        'guides/smart-contract-integration/07-supporting-meta-transactions',
-        
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Interface Integration',
-      items: [
-        'guides/interface-integration/01-using-the-api',
-        'guides/interface-integration/02-custom-interface-linking',
-        'guides/interface-integration/03-iframe-integration',
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Javascript SDK',
-      items: [
-        'guides/javascript-sdk/01-quick-start',
-        'guides/javascript-sdk/02-fetching-data',
-        'guides/javascript-sdk/03-pricing',
-        'guides/javascript-sdk/04-trading',
-        'guides/javascript-sdk/05-getting-pair-addresses',
-      ],
-    },
-  ],
-
-  reference: [
-    {
-      type: 'category',
-      label: 'API',
-      items: [
-        'reference/API/01-overview',
-        'reference/API/02-entities',
-        'reference/API/03-queries',    
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Governance',
-      items: [
-        'reference/Governance/05-governance-reference',   
-      ],
-    },
-    {
-      type: 'category',
-      label: 'Smart Contracts',
-      items: [
-        'reference/smart-contracts/01-factory',
-        'reference/smart-contracts/02-pair',
-        'reference/smart-contracts/03-pair-erc-20',
-        'reference/smart-contracts/04-library',
-        'reference/smart-contracts/05-router01',
-        'reference/smart-contracts/06-router02',
-        'reference/smart-contracts/07-common-errors',
-        
-      ],
-    },
-    {
-      type: 'category',
-      label: 'SDK',
-      items: [
-        'reference/SDK/01-getting-started',
-        'reference/SDK/02-token',
-        'reference/SDK/03-pair',
-        'reference/SDK/04-route',
-        'reference/SDK/05-trade',
-        'reference/SDK/06-fractions',
-        'reference/SDK/07-fetcher',
-        'reference/SDK/08-other-exports',
-        
-      ],
-    },
-  ],
 
 };
