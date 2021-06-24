f2d5987d6b13941bd2e87b0073aa008edeb22f22
Connor Martin <cnr.mrtn@gmail.com>
2021-06-24 15:47:21 -0400

    ported landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,85 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './styles.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="../docs/what-is-uniswap">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import styles from "./styles.module.css";
+import HomepageFeatures from "../components/HomepageFeatures";
+import "./styles.module.css";
+import styled from "@emotion/styled";
+
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  height: auto;
+  width: auto;
+`;
+
+const Card = styled.div`
+  display: flex;
+  min-height: 600px;
+  flex-wrap: wrap;
+  min-width: 350px;
+  flex-flow: column;
+  cursor: pointer;
+  background-color: ${({ color }) => color || `#fff`};
+  &:hover {
+    transform: translate(0%, -3%);
+    transition: 0.3s ease-out;
+  }
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  margin-top: auto;
+  margin-right: auto;
+`;
+
+const CardText = styled.div`
+  padding: 8px 8px 16px 8px;
+  margin-right: auto;
+`;
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    
+      title={`Uniswap Docs`}
+      description="Technical Documentation For The Uniswap Protocol"
+    >
+      <div>
+        <Row>
+          <Link to="./protocol/what-is-uniswap">
+            <Card color="#F5E7DD">
+              <Bottom>
+                <CardText>Concepts</CardText>
+              </Bottom>
+            </Card>
+          </Link>
+          <Link to="./sdk/landing">
+            <Card color="#EDDBF7">
+              <Bottom>
+                <CardText>Javascript SDK</CardText>
+              </Bottom>
+            </Card>
+          </Link>
+          <Link to="./protocol/reference/smart-contracts">
+            <Card color="#F1F1F2">
+              <Bottom>
+                <CardText>Smart Contract Reference</CardText>
+              </Bottom>
+            </Card>
+          </Link>
+          <Link to="/sdk/2.0.0/readme">
+            <Card color="#9C9990">
+              <Bottom>
+                <CardText>The V2 SDK</CardText>
+              </Bottom>
+            </Card>
+          </Link>
+        </Row>
+      </div>
     </Layout>
   );
 }
