81323415296f2ca72becf6fa936586dc9a949e01
dannydaniil <danny.daniil@uniswap.org>
2022-11-07 18:35:07 -0500

    uniswap analytics pkgs integration

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,23 +1,22 @@
 import React, { useEffect } from 'react'
 import { useLocation } from '@docusaurus/router'
 
-import { EventName, initializeAnalytics, sendAnalyticsEvent } from '@uniswap/analytics'
+import { initializeAnalytics } from '@uniswap/analytics'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+
+// Placeholder API key. Actual API key used in the proxy server
+const ANALYTICS_DUMMY_KEY = '00000000000000000000000000000000'
 
 // Default implementation, that you can customize
 export default function Root({ children }) {
   const location = useLocation()
   const { siteConfig } = useDocusaurusContext()
 
-  const analyticsUrl = siteConfig.customFields.analytics
+  const analyticsUrl = typeof siteConfig.customFields.analytics === 'string' ? siteConfig.customFields.analytics : null
+  initializeAnalytics(ANALYTICS_DUMMY_KEY, analyticsUrl)
 
   useEffect(() => {
     console.log('Location changed: ', location.pathname)
-  }, [location])
-
-  useEffect(() => {
-    initializeAnalytics(analyticsUrl)
-    sendAnalyticsEvent(EventName.EXPLORE_BANNER_CLICKED, {})
   }, [location])
 
   return <>{children}</>
