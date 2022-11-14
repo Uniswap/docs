15e4af315b50deef0c68b0540bcccda0ebbc00cc
dannydaniil <danny.daniil@uniswap.org>
2022-11-14 11:52:26 -0500

    update comment

--- src/theme/DocSidebarItem.tsx
+++ src/theme/DocSidebarItem.tsx
@@ -6,11 +6,11 @@ import { BrowserEvent, EventName } from '@uniswap/analytics-events'
 export default function DocSidebarItem(props: { item }) {
   return (
     <>
-      <TraceEvent events={[BrowserEvent.onClick]} element={props.item.label} name={EventName.MENU_CLICK}>
+      <TraceEvent events={[BrowserEvent.onClick]} element={props.item.label} name={EventName.MENU_CLICKED}>
         {/* Required for onClick to register */}
         <div
           onClick={(event) => {
-            // Required for Analytics to accurately determine the item clicked
+            //  Prevents the analytics event from being fired for encapsulating menu items, limiting to only the most granular element
             event.stopPropagation()
           }}
         >
