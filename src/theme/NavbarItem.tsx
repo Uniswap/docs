78379660266d1b188bb300e9ad62b920188096e2
dannydaniil <danny.daniil@uniswap.org>
2022-11-14 11:07:43 -0500

    adjust naming for version change

--- src/theme/NavbarItem.tsx
+++ src/theme/NavbarItem.tsx
@@ -4,7 +4,16 @@ import { useLocation } from '@docusaurus/router'
 import { TraceEvent } from '@uniswap/analytics'
 import { BrowserEvent, EventName } from '@uniswap/analytics-events'
 
+enum ProtocolVersion {
+  V1 = 'V1',
+  V2 = 'V2',
+  V3 = 'V3',
+}
+
 const getElementName = (version: string, elementName: string) => {
+  if (elementName in ProtocolVersion) {
+    return elementName
+  }
   return version + ' ' + elementName
 }
 
@@ -18,12 +27,12 @@ export default function NavbarItem(props: { className: string; label: string }) 
   const versionDoc = pathname.split('/')
   let activeNav: null | string = null
 
-  if (versionDoc[2] === 'V2' || versionDoc[2] === '2.0.0') {
-    activeNav = 'V2'
-  } else if (versionDoc[2] === 'V1' || versionDoc[2] === '1.0.0') {
-    activeNav = 'V1'
+  if (versionDoc[2] === ProtocolVersion.V2 || versionDoc[2] === '2.0.0') {
+    activeNav = ProtocolVersion.V2
+  } else if (versionDoc[2] === ProtocolVersion.V1 || versionDoc[2] === '1.0.0') {
+    activeNav = ProtocolVersion.V1
   } else {
-    activeNav = 'V3'
+    activeNav = ProtocolVersion.V3
   }
 
   return (
