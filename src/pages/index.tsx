f1c96281839ac00f8527ebd66b85128f71f25e43
dannydaniil <danny.daniil@uniswap.org>
2022-11-18 09:48:24 -0500

    text hierarchy for getting started

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -396,7 +396,7 @@ export default function Home() {
                     <Card key={action.title} style={{ marginBottom: '1rem' }}>
                       <LinkRow>
                         <div style={{ display: 'flex', alignItems: 'center' }}>
-                          <h3 style={{ marginBottom: '0rem', fontWeight: 400 }}>{action.title}</h3>
+                          <h3 style={{ marginBottom: '0rem' }}>{action.title}</h3>
                         </div>
                         <svg
                           style={{ width: '24px', opacity: 0.2 }}
@@ -407,7 +407,7 @@ export default function Home() {
                           <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
                         </svg>
                       </LinkRow>
-                      <p style={{ marginBottom: '0rem', fontWeight: 200 }}>{action.text}</p>
+                      <p style={{ marginBottom: '0rem', fontWeight: 300 }}>{action.text}</p>
                     </Card>
                   </Link>
                 </TraceEvent>
@@ -517,7 +517,6 @@ export default function Home() {
                     }}
                   >
                     <h3 style={{ marginBottom: '0rem', fontWeight: 300 }}>{action.title}</h3>
-
                     <svg
                       style={{ width: '16px', opacity: 0.2 }}
                       xmlns="http://www.w3.org/2000/svg"
