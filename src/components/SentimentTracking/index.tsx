0c4f0efc330ef479e29534ff9d2620cf9626e0ac
dannydaniil <danny.daniil@uniswap.org>
2022-11-22 21:11:14 -0500

    add colors from interface

--- src/components/SentimentTracking/index.tsx
+++ src/components/SentimentTracking/index.tsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { TraceEvent } from '@uniswap/analytics'
 import { BrowserEvent } from '@uniswap/analytics-events'
 import { ThumbsUp, ThumbsDown } from 'react-feather'
+import { colors } from '../../theme/color'
 
 import styled from '@emotion/styled'
 
@@ -10,9 +11,6 @@ const ANALYTICS_SECTION_NAME = 'RATING'
 const ANALYTICS_POSITIVE_ELEMENT_NAME = 'POSITIVE_SENTIMENT'
 const ANALYTICS_NEGATIVE_ELEMENT_NAME = 'NEGATIVE_SENTIMENT'
 const ANALYTICS_EVENT_NAME = 'RATING_CLICKED'
-const POSITIVE_SENTIMENT_COLOR = 'green'
-const NEGATIVE_SENTIMENT_COLOR = 'red'
-const UNSELECTED_SENTIMENT_COLOR = 'clear'
 
 enum Sentiment {
   NEGATIVE = 'NEGATIVE',
@@ -33,20 +31,20 @@ const SentimentContainer = styled.div`
 `
 
 const PositiveSentimentIcon = styled(ThumbsUp)<{ isSelect: boolean }>`
-  fill: ${(props) => (props.isSelect ? POSITIVE_SENTIMENT_COLOR : UNSELECTED_SENTIMENT_COLOR)};
+  fill: ${(props) => (props.isSelect ? colors.greenVibrant : 'transparent')};
   opacity: ${(props) => (props.isSelect ? 1 : 0.5)};
 
   &:hover {
-    fill: ${POSITIVE_SENTIMENT_COLOR};
+    fill: ${colors.greenVibrant};
   }
 `
 
 const NegativeSentimentIcon = styled(ThumbsDown)<{ isSelect: boolean }>`
-  fill: ${(props) => (props.isSelect ? NEGATIVE_SENTIMENT_COLOR : UNSELECTED_SENTIMENT_COLOR)};
+  fill: ${(props) => (props.isSelect ? colors.redVibrant : 'transparent')};
   opacity: ${(props) => (props.isSelect ? 1 : 0.5)};
 
   &:hover {
-    fill: ${NEGATIVE_SENTIMENT_COLOR};
+    fill: ${colors.redVibrant};
   }
 `
 
---
+++ src/theme/color.ts
@@ -0,0 +1,5 @@
+// Based mostly on https://github.com/Uniswap/interface/blob/main/src/theme/index.tsx
+export const colors = {
+  redVibrant: '#F14544',
+  greenVibrant: '#5CFE9D',
+}
