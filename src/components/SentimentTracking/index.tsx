cf01fec12a4a29228b390036752861a4acb4207c
Sohee Lim <limsohee1002@gmail.com>
2024-10-11 10:42:10 -0400

    fix: remove custom styles as much as possible

--- src/components/SentimentTracking/index.tsx
+++ src/components/SentimentTracking/index.tsx
@@ -1,57 +1,13 @@
-import styled from '@emotion/styled'
 import { TraceEvent } from '@uniswap/analytics'
 import { BrowserEvent, DocsSentiment, DocsSentimentSection, SharedEventName } from '@uniswap/analytics-events'
 import React, { useCallback, useState } from 'react'
 import { Frown, Meh, Smile } from 'react-feather'
-
-import { colors } from '../../theme/color'
-import { Opacity } from '../../theme/style'
 
 enum Sentiment {
   NEGATIVE = 'NEGATIVE',
   NEUTRAL = 'NEUTRAL',
   POSITIVE = 'POSITIVE',
 }
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-`
-
-const PositiveSentimentIcon = styled(Smile)<{ selected: boolean }>`
-  fill: ${(props) => (props.selected ? colors.greenVibrant : 'transparent')};
-  opacity: ${(props) => (props.selected ? Opacity.FULL : Opacity.MEDIUM)};
-
-  &:hover {
-    fill: ${colors.greenVibrant};
-  }
-`
-
-const NegativeSentimentIcon = styled(Frown)<{ selected: boolean }>`
-  fill: ${(props) => (props.selected ? colors.redVibrant : 'transparent')};
-  opacity: ${(props) => (props.selected ? Opacity.FULL : Opacity.MEDIUM)};
-
-  &:hover {
-    fill: ${colors.redVibrant};
-  }
-`
-
-const NeutralSentimentIcon = styled(Meh)<{ selected: boolean }>`
-  fill: ${(props) => (props.selected ? colors.yellowVibrant : 'transparent')};
-  opacity: ${(props) => (props.selected ? Opacity.FULL : Opacity.MEDIUM)};
-  margin: 0 0.2rem;
-
-  &:hover {
-    fill: ${colors.yellowVibrant};
-  }
-`
-
-const StyledTextDiv = styled.div`
-  font-size: 1rem;
-  padding-right: 0.5rem;
-`
 
 export default function SentimentTracking({ analyticsSection }: { analyticsSection: DocsSentimentSection }) {
   const [selectedSentiment, setSelectedSentiment] = useState<null | Sentiment>(null)
@@ -62,20 +18,20 @@ export default function SentimentTracking({ analyticsSection }: { analyticsSecti
   )
 
   return (
-    <Container>
-      <StyledTextDiv>Helpful?</StyledTextDiv>
+    <div>
+      <div>Helpful?</div>
       <TraceEvent
         element={DocsSentiment.POSITIVE_SENTIMENT}
         name={SharedEventName.SENTIMENT_SUBMITTED}
         events={[BrowserEvent.onClick]}
         section={analyticsSection}
       >
-        <PositiveSentimentIcon
+        {/* <PositiveSentimentIcon
           selected={isSentimentSelected(Sentiment.POSITIVE)}
           onClick={() => {
             setSelectedSentiment(Sentiment.POSITIVE)
           }}
-        />
+        /> */}
       </TraceEvent>
       <TraceEvent
         element={DocsSentiment.NEUTRAL_SENTIMENT}
@@ -83,12 +39,12 @@ export default function SentimentTracking({ analyticsSection }: { analyticsSecti
         events={[BrowserEvent.onClick]}
         section={analyticsSection}
       >
-        <NeutralSentimentIcon
+        {/* <NeutralSentimentIcon
           selected={isSentimentSelected(Sentiment.NEUTRAL)}
           onClick={() => {
             setSelectedSentiment(Sentiment.NEUTRAL)
           }}
-        />
+        /> */}
       </TraceEvent>
       <TraceEvent
         element={DocsSentiment.NEGATIVE_SENTIMENT}
@@ -96,13 +52,13 @@ export default function SentimentTracking({ analyticsSection }: { analyticsSecti
         events={[BrowserEvent.onClick]}
         section={analyticsSection}
       >
-        <NegativeSentimentIcon
+        {/* <NegativeSentimentIcon
           selected={isSentimentSelected(Sentiment.NEGATIVE)}
           onClick={() => {
             setSelectedSentiment(Sentiment.NEGATIVE)
           }}
-        />
+        /> */}
       </TraceEvent>
-    </Container>
+    </div>
   )
 }
--- src/theme/DocBreadcrumbs.tsx
+++ src/theme/DocBreadcrumbs.tsx
@@ -1,28 +1,17 @@
-import styled from '@emotion/styled'
 import OriginalDocBreadcrumbs from '@theme-original/DocBreadcrumbs'
 import { DocsSentimentSection } from '@uniswap/analytics-events'
 import React from 'react'
 
 import SentimentTracking from '../components/SentimentTracking'
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`
-
-const SentimentTrackingContainer = styled.div`
-  margin-bottom: 0.7rem;
-`
-
 export default function DocBreadcrumbs(props) {
+
   return (
-    <Container>
+    <div>
       <OriginalDocBreadcrumbs {...props} />
-      <SentimentTrackingContainer>
+      <div>
         <SentimentTracking analyticsSection={DocsSentimentSection.TOP_SECTION} />
-      </SentimentTrackingContainer>
-    </Container>
+      </div>
+    </div>
   )
 }
