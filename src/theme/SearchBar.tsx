2f3cacaa7e19b4cc2f40e5fd77f9044ce77d69a8
PARMESHWARPANWAR <parmeshwarpanwar777@gmail.com>
2025-01-28 19:49:53 +0530

    resolve bug for searchBar and home page

--- src/theme/SearchBar.tsx
+++ src/theme/SearchBar.tsx
@@ -1,17 +1,35 @@
+import styled from '@emotion/styled'
 import OriginalSearchBar from '@theme-original/SearchBar'
 import { TraceEvent } from '@uniswap/analytics'
 import { BrowserEvent, SharedEventName } from '@uniswap/analytics-events'
 import React from 'react'
 
+const SearchWrapper = styled.div`
+  div[class*="searchBox_"] {
+    position: static !important;
+    padding: 0 !important;
+    margin: 0 !important;
+  }
+
+  .DocSearch.DocSearch-Button {
+    position: relative !important;
+    top: auto !important;
+    left: auto !important;
+    right: auto !important;
+    bottom: auto !important;
+    transform: none !important;
+  }
+`
+
 export default function SearchBarWithAnalytics(props) {
   return (
-    <>
-      <TraceEvent events={[BrowserEvent.onClick]} name={SharedEventName.SEARCH_BAR_CLICKED}>
-        {/* Required for onClick to register */}
-        <div>
-          <OriginalSearchBar {...props} />
-        </div>
-      </TraceEvent>
-    </>
+    <TraceEvent 
+      events={[BrowserEvent.onClick]} 
+      name={SharedEventName.SEARCH_BAR_CLICKED}
+    >
+      <SearchWrapper>
+        <OriginalSearchBar {...props} />
+      </SearchWrapper>
+    </TraceEvent>
   )
 }
