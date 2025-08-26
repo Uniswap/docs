366e05a2ac8fbb3970d15ce885df0877b54de7a2
Babuaravind Gururaj <gururaj.b@northeastern.edu>
2025-08-26 15:14:49 -0400

    Removed smart-widget related documentation as it is no longer mainted and to eliminate confusion and removed the sentiment tracker

--- src/theme/DocPaginator.tsx
+++ src/theme/DocPaginator.tsx
@@ -2,14 +2,9 @@ import OriginalDocPaginator from '@theme-original/DocPaginator'
 import { DocsSentimentSection } from '@uniswap/analytics-events'
 import React from 'react'
 
-import SentimentTracking from '../components/SentimentTracking'
-
 export default function DocPaginator(props) {
   return (
     <>
-      <div>
-        <SentimentTracking analyticsSection={DocsSentimentSection.BOTTOM_SECTION} />
-      </div>
       <OriginalDocPaginator {...props} />
     </>
   )
